cd02a71c2dca80f13ff856967bb4e7c938305a15
Gnlow <gnlow@protonmail.com>
2023-08-04 21:54:16 +0900

    yee

--- web/src/lib/Map.svelte
+++ web/src/lib/Map.svelte
@@ -4,6 +4,8 @@
 
     import world from "../assets/world_min.svg"
 
+    import worldPolygon from "../assets/5282.geo.json"
+
     let mapElement: HTMLElement
     let map
 
@@ -14,6 +16,10 @@
         if (browser) {
             const L = await import("leaflet")
             await import("leaflet-editable")
+            // @ts-ignore
+            await import("leaflet-path-drag")
+            // @ts-ignore
+            await import("leaflet-snap")
 
             map = L.map(mapElement, {
                 crs: L.CRS.EPSG4326,
@@ -23,6 +29,23 @@
             L.imageOverlay(world, [[-90, 0], [90, 360]]).addTo(map)
             L.imageOverlay(world, [[-90, 360], [90, 720]]).addTo(map)
             map.fitBounds([[-90, 0], [90, 360]])
+
+            const polygon = L.geoJSON(
+                worldPolygon as any,
+                {
+                    onEachFeature(feature, layer) {
+                        layer.on({
+                            click(e) {
+                                // @ts-ignore
+                                layer.enableEdit()
+                            }
+                        })
+                    }
+                 }
+            ).addTo(map)
+
+            // @ts-ignore
+            //polygon.getLayers().forEach(l => l.enableEdit())
 
             map.on("mousemove", ({latlng}) => {
                 lat = latlng.lat
