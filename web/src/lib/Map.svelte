5332c0305744774557e68a1eccb90321dd3c1368
Gnlow <gnlow@protonmail.com>
2023-08-04 17:53:58 +0900

    it works

--- web/src/lib/Map.svelte
+++ web/src/lib/Map.svelte
@@ -7,22 +7,32 @@
     let mapElement: HTMLElement
     let map
 
-    let worldElement: SVGElement
+    let lat: number
+    let lng: number
 
     onMount(async () => {
         if (browser) {
             const L = await import("leaflet")
 
             map = L.map(mapElement, {
-                crs: L.CRS.Simple
+                crs: L.CRS.EPSG4326
             })
             L.imageOverlay(
                     world,
-                    [[0, 0], [3840, 2160]]
+                    [[-90, 0], [90, 360]]
                 ).addTo(map)
-            map.fitBounds([[0, 0], [3840, 2160]])
+            map.fitBounds([[-90, 0], [90, 360]])
+
+            map.on("mousemove", ({latlng}) => {
+                lat = latlng.lat
+                lng = latlng.lng
+            })
         } else { console.log("HO") }
     })
+
+    const format = (n: number) =>
+        n.toFixed(5).substring(0, 5)
+    
 </script>
 
 <style>
@@ -33,4 +43,5 @@
     }
 </style>
 
+({format(lng || 0)}, {format(lat || 0)})
 <div class="map" bind:this={mapElement}/>
