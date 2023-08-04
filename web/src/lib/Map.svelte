6e89132fafabe0ad711c0c053cf937fbaec85adc
Gnlow <gnlow@protonmail.com>
2023-08-04 16:17:52 +0900

    nooo

---
+++ web/src/lib/Map.svelte
@@ -0,0 +1,20 @@
+<script>
+    const window = globalThis
+
+    import { LeafletMap, ImageOverlay } from "svelte-leafletjs"
+
+    let leafletMap
+</script>
+
+<LeafletMap
+    bind:this={leafletMap}
+    options={{
+        center: [0, 0],
+        zoom: 1,
+    }}
+>
+    <ImageOverlay
+        imageUrl="temp/5282/world.png"
+        bounds={[[0,0],[3840,2160]]}
+    />
+</LeafletMap>
